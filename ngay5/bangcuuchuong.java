fb3ab309b867237caa90664b00b1a56de83d8d0a
duda <rongthien093@gmail.com>
2025-08-02 17:52:06 +0700

    bang-cuu-chuong

--- ngay5/bangcuuchuong.java
+++ ngay5/bangcuuchuong.java
@@ -1,5 +1,12 @@
 package ngay5;
-
+//bảng cửu chương ngon;
 public class bangcuuchuong {
-    
+    public static void main(String[] args) {
+        for(int i = 2;i <= 9;i++ ){
+            System.out.println("bang cuu chuong " + i );
+            for(int j = 1 ; j <=10;j++){
+                System.out.println(i +" x " + j + " = " +i*j);
+            }
+        }
+    }
 }
